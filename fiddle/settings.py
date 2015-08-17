f1262a2f44c851d7ba50d6e1838bf23c1ac122ca
Paul Imisi <pimisi@yahoo.com>
2015-08-17 22:19:29 +0200

    - Additional Modification on the Base Service Model

--- fiddle/settings.py
+++ fiddle/settings.py
@@ -113,23 +113,23 @@ WSGI_APPLICATION = 'fiddle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fiddle',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fiddle',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Internationalization
--- static/js/_services/factories/base.service.model.js
+++ static/js/_services/factories/base.service.model.js
@@ -9,20 +9,10 @@
 
     function BaseServiceModel($resource, API_PREFERRED_KEYS, $rootScope, APIHelper, Base64) {
 
-        //var serviceObject = {};
-        var apiUriObject = {};
-
-        function addAuthorizationHeader(data, headersGetter) {
-            // as per HTTP accounts spec [1], credentials must be
-            // encoded in base64.
-            var headers = headersGetter();
-            //headers['Authorization'] = ('Basic ' + Base64.encode(data.username + ':' + data.password))
-        }
-
         var BaseServiceModel = function (requestedService, param) {
 
-            //this.serviceObject = {};
             this.responsePayload = null;
+            this.responseError = null;
 
             this.apiUriObject = APIHelper.getAPIServiceURIObject(
                 API_PREFERRED_KEYS.main,
@@ -32,16 +22,16 @@
             );
 
             /* APIHelper.getServiceURIObject("development", "mock", requestedService, param,
-                function (serviceURIObject) {
-                    self.apiUriObject = serviceURIObject;
-                    apiUriObject = self.apiUriObject;
+             function (serviceURIObject) {
+             self.apiUriObject = serviceURIObject;
+             apiUriObject = self.apiUriObject;
 
-                    console.log(self.apiUriObject);
+             console.log(self.apiUriObject);
 
 
-                    //fetchCallback(apiUriObject);
-                    return serviceURIObject;
-                });*/
+             //fetchCallback(apiUriObject);
+             return serviceURIObject;
+             });*/
         }
 
         /**
@@ -49,28 +39,7 @@
          * This method uses a fallback logic so that if the primary source of the call fails
          * it would make a secondary call to retrieve mock data of the exact same payload structure
          */
-        //var JSONRequest = $resource(apiUriObject["main"], null, {
-        //    fetchJSONObject: {
-        //        method: 'GET',
-        //        transformRequest: addAuthorizationHeader
-        //    },
-        //    fetchJSONArray: {
-        //        method: 'GET',
-        //        transformRequest: addAuthorizationHeader,
-        //        isArray: true
-        //    },
-        //    postJSONObject: {
-        //        method: 'POST',
-        //        transformRequest: addAuthorizationHeader
-        //    },
-        //    postJSONArray: {
-        //        method: 'POST',
-        //        transformRequest: addAuthorizationHeader,
-        //        isArray: true
-        //    }
-        //});
-
-        BaseServiceModel.prototype.JSONRequest = function(sourceURI) {
+        BaseServiceModel.prototype.JSONRequest = function (sourceURI) {
 
             console.log(sourceURI);
 
@@ -105,28 +74,49 @@
                 self.responsePayload = data;
 
                 //return data;
-            }, function(data) {
-                if (data.data.status == 404) {
+            }, function (response) {
+                var __response = getResponseError(response);
+
+                if (__response.responseData == null || __response.status == 404) {
                     // make secondary call
-                    return self.JSONRequest(self.apiUriObject["fallback"]).fetchJSONObject().$promise.then(function (data) {
-                        self.responsePayload = data;
+                    return self.JSONRequest(self.apiUriObject["fallback"]).fetchJSONObject()
+                        .$promise.then(function (data) {
+                            self.responsePayload = data;
 
-                        //return data;
-                    }, function(data) {
-                        console.log("Fallback Other Method");
-                    }).catch(function(data) {
-                        console.log("Fallback Failed");
+                            //return data;
+                        }, function (response) {
+                            console.log("Fallback Other Method");
+                            self.responseError = getResponseError(response);
 
-                        //return null;
-                    });
+                        }).catch(function (response) {
+                            console.log("Fallback Failed");
+                            self.responseError = getResponseError(response);
+                            //return null;
+                        });
+                } else {
+                    this.responseError = getResponseError(response);
                 }
                 console.log("Other Method");
 
-            }).catch(function(data) {
+            }).catch(function (response) {
                 console.log("Failed");
 
                 //return null;
             });
+        }
+
+        function addAuthorizationHeader(data, headersGetter) {
+            // as per HTTP accounts spec [1], credentials must be
+            // encoded in base64.
+            var headers = headersGetter();
+            //headers['Authorization'] = ('Basic ' + Base64.encode(data.username + ':' + data.password))
+        }
+
+        function getResponseError(response) {
+            var responseData = response.hasOwnProperty('data') ? response.data : null;
+            var status = response.hasOwnProperty('status') ? response.status : 0;
+            var statusText = response.hasOwnProperty('statusText') ? response.statusText : '';
+            return {responseData: responseData, status: status, statusText: statusText};
         }
 
         return BaseServiceModel;
